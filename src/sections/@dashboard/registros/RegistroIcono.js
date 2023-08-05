0b99218b77d9b46b63ce5a6d6915a2c4f323880f
FernandoMichea90 <fmicheam@gmail.com>
2023-08-05 03:15:54 -0400

    cambios:RegistroIcono.js

--- src/sections/@dashboard/registros/RegistroIcono.js
+++ src/sections/@dashboard/registros/RegistroIcono.js
@@ -1,37 +1,22 @@
 import React from 'react';
 import Picker, { Emoji, EmojiStyle } from 'emoji-picker-react';
-import PropTypes from 'prop-types';
-import {Button, IconButton} from '@mui/material';
+import { Button, IconButton } from '@mui/material';
 
-
-// RegistroIcono.propTypes = {
-//     selectedEmoji: PropTypes.string,
-//     onClickIcono: PropTypes.func,
-//     onEmojiClick: PropTypes.func,
-//     showPicker: PropTypes.bool,
-// };
-
-const RegistroIcono = ({selectedEmoji,onClickIcono,onEmojiClick,showPicker}) => {
+const RegistroIcono = ({ selectedEmoji, onClickIcono, onEmojiClick, showPicker }) => {
   return (
-        <div>
-        {selectedEmoji ? (
-            <IconButton onClick={onClickIcono}>
-            <Emoji
-                unified={selectedEmoji}
-                emojiStyle={EmojiStyle.APPLE}
-                size={22}   
-            />
-            </IconButton>
-        ) :
-            <Button variant="outlined" color="inherit" onClick={onClickIcono} >
-            Añadir Icono
-            </Button>
-        }
-        {showPicker &&
-            <Picker onEmojiClick={onEmojiClick}  width="100%" ></Picker>
-        }
-        </div>
-  )
-}
+    <div>
+      {selectedEmoji ? (
+        <IconButton onClick={onClickIcono}>
+          <Emoji unified={selectedEmoji} emojiStyle={EmojiStyle.APPLE} size={22} />
+        </IconButton>
+      ) : (
+        <Button variant="outlined" color="inherit" onClick={onClickIcono}>
+          Añadir Icono
+        </Button>
+      )}
+      {showPicker && <Picker onEmojiClick={onEmojiClick} width="100%" />}
+    </div>
+  );
+};
 
-export default RegistroIcono
+export default RegistroIcono;
